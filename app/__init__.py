db6e946ffb53e76a249fd2a821c035ed8ba851e1
ganeshys <1964100035@qq.com>
2018-07-30 22:25:43 +0800

    firstcommit

---
+++ app/__init__.py
@@ -0,0 +1,27 @@
+# coding:utf8
+import os
+
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+import pymysql
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:root@127.0.0.1:3306/movie"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
+app.config["SECRET_KEY"] = '4725ba65fe334bcd8507352dc9e3dfe5'
+app.config["UP_DIR"] = os.path.join(os.path.abspath(os.path.dirname(__file__)), "static/uploads")
+app.config["FC_DIR"] = os.path.join(os.path.abspath(os.path.dirname(__file__)), "static/uploads/users/")
+app.debug = True
+db = SQLAlchemy(app)
+
+from app.home import home as home_blueprint
+from app.admin import admin as admin_blueprint
+
+app.register_blueprint(home_blueprint)
+app.register_blueprint(admin_blueprint, url_prefix="/admin")
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template("home/404.html"), 404
